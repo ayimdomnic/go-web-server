878ef7d4aca25c06a8741d7c17b8225496180d10
Odhiambo Dormnic <ayimdomnic@outlook.com>
2022-09-10 14:33:21 +0300

    chore: fix app booting

--- helpers/env.go
+++ helpers/env.go
@@ -1,17 +1,9 @@
 package helpers
 
 import (
-	"log"
 	"os"
-	"github.com/joho/godotenv"
 )
 
 func GoDotEnvVariableKey(key string) string {
-	err := godotenv.Load(".env", "env-example") 
-
-	if err != nil {
-		log.Fatal("Error loading the env file")
-	}
-
 	return os.Getenv(key)
 }
